4e434aedab1e6ee03af0ef7ff2f9b6a804007462
Agustin Procofio <131788963+AgustinProcofio@users.noreply.github.com>
2024-08-13 10:18:03 -0300

    Resta, division

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,22 @@
-﻿public class Addition
+﻿Console.WriteLine(Addition.Add(7, 2));
+Console.WriteLine(Substraction.Substract(10, 1));
+Console.WriteLine(Multiplication.Multiply(3, 3));
+Console.WriteLine(Division.Divide(18, 2));
+
+public class Addition
 {
    public static int Add(int a, int b)
    {
        return a + b;
    }
 }
-
+public class Substraction
+{
+   public static int Substract(int a, int b)
+   {
+       return a - b;
+   }
+}
 public class Multiplication
 {
    public static int Multiply(int a, int b)
@@ -13,10 +24,12 @@ public class Multiplication
        return a * b;
    }
 }
+public class Division
+{
+   public static double Divide(int a, int b)
+   {
+       return (double)a / b;
+   }
+}
 
 
-
-//Console.WriteLine(Addition.Add(7, 2));
-//Console.WriteLine(Substraction.Substract(10, 1));
-//Console.WriteLine(Multiplication.Multiply(3, 3));
-//Console.WriteLine(Division.Divide(18, 2));
